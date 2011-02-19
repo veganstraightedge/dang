1ae7302ecc9a3ac08bca47bcd00e53a9d6a6d031
Shane Becker <veganstraightedge@gmail.com>
2011-02-19 02:07:52 -0800

    array to attr values syntax

--- test/test_array_classes.rb
+++ test/test_array_classes.rb
@@ -1,20 +1,9 @@
 require "helper"
 
 describe "array classes" do
-  it "should example an array into space separate classes" do
-    # An Array of classes that is auto joined space separated
-    # <body#id.class.class2.class3
-    #
-    # instead as
-    #
-    # <- classes = %w(class class2 class3) ->
-    # <body#id.[= classes =]
-    # <body#id.<= classes =>
-    # <body#id.#{ classes }
-    # <body#id[class=<= classes =>]
-    # <body#id[class=#{ classes }]
-    # <body#id[class={ classes }]
-
-    # TODO think about this syntax
+  it "joins an array of items into space separate attribute values" do
+    classes = %w(snap crackle pop)
+    Dang::it(<"body#mitch[class={ classes }]").must_equal   "<body id='mitch' class='snap crackle pop'>"
+    Dang::it(<"body#mitch[class={ %w(a b c) }]").must_equal "<body id='mitch' class='a b c'>"
   end
 end
