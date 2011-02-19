adb810857584b9f3462bae1a547490a551c96e53
Shane Becker <veganstraightedge@gmail.com>
2011-02-19 01:22:27 -0800

    singular comment

--- test/test_comment.rb
+++ test/test_comment.rb
@@ -1,6 +1,7 @@
 require 'helper'
+require 'comment'
 
-describe "comments" do
+describe Comment do
   it "transforms DANG //comment comments into html comments" do
     Dang::it(" //comment").must_equal           "<!-- comment -->"
     Dang::it("<b BOLD b> //comment").must_equal "<b>BOLD</b> <!-- comment -->"
