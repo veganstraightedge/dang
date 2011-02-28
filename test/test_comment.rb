6dbea4a6706beae60a2c7b8942c27aa7eaab2ee3
Shane Becker <veganstraightedge@gmail.com>
2011-02-28 09:54:01 -0800

    fixed test typo

--- test/test_comment.rb
+++ test/test_comment.rb
@@ -2,7 +2,7 @@ require 'helper'
 require 'comment'
 
 describe Comment do
-  it "transforms DANG </ comment /> comments into html comments" do
+  it "transforms DANG <! comment !> comments into html comments" do
     Dang::it("<! comment !>").must_equal "<!-- comment -->"
   end
 end
