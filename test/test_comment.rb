dbbb8012c0dba1e3596ebf7181fa3d2019eca21d
Shane Becker <veganstraightedge@gmail.com>
2011-02-28 09:53:29 -0800

    changed comment syntax

--- test/test_comment.rb
+++ test/test_comment.rb
@@ -3,7 +3,6 @@ require 'comment'
 
 describe Comment do
   it "transforms DANG </ comment /> comments into html comments" do
-    Dang::it("</ comment />").must_equal           "<!-- comment -->"
-    Dang::it("<b BOLD b> //comment").must_equal "<b>BOLD</b> <!-- comment -->"
+    Dang::it("<! comment !>").must_equal "<!-- comment -->"
   end
 end
--- test/test_conditional_comment.rb
+++ test/test_conditional_comment.rb
@@ -1,26 +1,25 @@
 require 'helper'
 require 'conditional_comment'
 
-# HAML like conditional comments, but with // instead of /, and with closers
-# //[if IE]
-# //[end]
+# <! [if IE] !>
+# <! [end] !>
 #
 # <!--[if IE]>
 # <![endif]-->
 
 describe ConditionalComment do
   it "transforms IE conditional comments" do
-    Dang::it("//[if IE]\n...\n//[end]").must_equal "<!--[if IE]>\n...\n<![endif]-->"
+    Dang::it("<! [if IE] !>\n...\n<! [end] !>").must_equal "<!--[if IE]>\n...\n<![endif]-->"
   end
 
   it "transforms IE6 conditional comments" do
-    Dang::it("//[if IE6]\n...\n//[end]").must_equal "<!--[if IEe]>\n...\n<![endif]-->"
+    Dang::it("<! [if IE6]\n...\n<! [end] !>").must_equal "<!--[if IEe]>\n...\n<![endif]-->"
   end
 
   it "does all the permutations of IE conditional comments"
-  it "transform IE conditional comments for non IE"
+  it "transforms IE conditional comments for non IE"
 
   it "transforms one line conditional comments" do
-    Dang::it("//[if IE]...//[end]").must_equal "<!--[if IE]>...<![endif]-->"
+    Dang::it("<! [if IE]...<! [end] !>").must_equal "<!--[if IE]>...<![endif]-->"
   end
 end
