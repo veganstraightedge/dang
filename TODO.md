85430bf2cc6378efc1ba3b6e7af9fc1a47d1c9cb
Shane Becker <veganstraightedge@gmail.com>
2011-02-28 10:10:16 -0800

    nested data attributes convenience syntax

--- TODO.md
+++
@@ -1,1 +0,0 @@
-data attrs convenience nesting
---
+++ test/test_data_attribute.rb
@@ -0,0 +1,12 @@
+require 'helper'
+require 'data_attribute'
+
+describe DataAttribute do
+  it "transforms normal data attributes comments" do
+    Dang::it("<span[data-foo=party][data-bar=time] now span>").must_equal "<span data-foo='party' data-bar='time'>now</span>"
+  end
+
+  it "transforms nested data attributes comments" do
+    Dang::it("<span[data[foo=party][bar=time]] now span>").must_equal "<span data-foo='party' data-bar='time'>now</span>"
+  end
+end
