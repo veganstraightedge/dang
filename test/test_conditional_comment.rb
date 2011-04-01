00009ef3e8ef847ef2361063a02cb3255703a0d1
Shane Becker <veganstraightedge@gmail.com>
2011-03-31 18:48:38 -0700

    VERY simple conditional comment start/end parsing

--- test/test_conditional_comment.rb
+++ test/test_conditional_comment.rb
@@ -6,19 +6,27 @@ require "helper"
 # <!--[if IE]>
 # <![endif]-->
 
-# describe "conditional comments" do
+describe "conditional comments" do
 #   it "transforms IE conditional comments" do
 #     Dang.it("<! [if IE] !>\n...\n<! [end] !>").must_equal "<!--[if IE]>\n...\n<![endif]-->"
 #   end
 # 
 #   it "transforms IE6 conditional comments" do
-#     Dang.it("<! [if IE6]\n...\n<! [end] !>").must_equal "<!--[if IEe]>\n...\n<![endif]-->"
+#     Dang.it("<! [if IE6] !>\n...\n<! [end] !>").must_equal "<!--[if IEe]>\n...\n<![endif]-->"
 #   end
 # 
 #   it "does all the permutations of IE conditional comments"
 #   it "transforms IE conditional comments for non IE"
-# 
-#   it "transforms one line conditional comments" do
-#     Dang.it("<! [if IE]...<! [end] !>").must_equal "<!--[if IE]>...<![endif]-->"
-#   end
-# end
+
+  # it "transforms one line conditional comments" do
+  #   Dang.it("<! [if IE] !>...<! [end] !>").must_equal "<!--[if IE]>...<![endif]-->"
+  # end
+
+  it "transforms DANG conditional comment start into HTML conditional comment start" do
+    Dang.it("<! [if IE] !>").must_equal "<!--[if IE]>"
+  end
+
+  it "transforms DANG conditional comment closers into HTML conditional comment closers" do
+    Dang.it("<! [end] !>").must_equal "<![endif]-->"
+  end
+end
