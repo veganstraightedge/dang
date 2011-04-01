1ec5caa5d5c71a08e819bf9398bde4bf62988513
Shane Becker <veganstraightedge@gmail.com>
2011-03-31 18:16:16 -0700

    multiline comment test

--- test/test_comment.rb
+++ test/test_comment.rb
@@ -4,4 +4,12 @@ describe "comments" do
   it "transforms DANG <! comment !> comments into html comments" do
     Dang.it("<! comment !>").must_equal "<!-- comment -->"
   end
+
+  it "transforms multiline DANG <! comment !> comments into multiline html comments" do
+    Dang.it("<!
+    comment
+    !>").must_equal "<!--
+    comment
+    -->"
+  end
 end
