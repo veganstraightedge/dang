b1f9b8c097b42c44dff9fb4a8145f86e8ca2bd47
Shane Becker <veganstraightedge@gmail.com>
2011-03-31 20:57:50 -0700

    updated the conditional comment regex a bit

--- spec/conditional_comment_spec.rb
+++ spec/conditional_comment_spec.rb
@@ -17,29 +17,36 @@ describe "conditional comments" do
     Dang.it("<! [if IE] !>abc").must_equal    "<!--[if IE]>abc"
     Dang.it("<! [if IE]      !>").must_equal  "<!--[if IE]>"
     Dang.it("<! [if !IE] !>").must_equal      "<!--[if !IE]>"
+    Dang.it("<! [if !IE 6] !>").must_equal    "<!--[if !IE 6]>"
+    Dang.it("<! [if !IE 5.5] !>").must_equal  "<!--[if !IE 5.5]>"
     Dang.it("<! [if IE 5] !>").must_equal     "<!--[if IE 5]>"
     Dang.it("<! [if IE 5.5] !>").must_equal   "<!--[if IE 5.5]>"
     Dang.it("<! [if IE 5.50] !>").must_equal  "<!--[if IE 5.50]>"
     Dang.it("<! [if IE 6] !>").must_equal     "<!--[if IE 6]>"
   end
 
-  it "transforms DANG conditional comment closers into HTML conditional comment closers" do
+  it "transforms DANG conditional comment closers" do
     Dang.it("<! [end] !>").must_equal "<![endif]-->"
   end
 
-  it "transforms DANG conditional comment closers with whitespace into HTML conditional comment closers" do
+  it "transforms DANG conditional comment closers with whitespace" do
     Dang.it("<!      [end]    !>").must_equal "<![endif]-->"
   end
 
-  # it "transforms DANG conditional comment closers into HTML conditional comment closers" do
+  # it "transforms conditional comments with boolean operators" do
+  #   Dang.it("<! [if (gt IE 5)&(lt IE 7)] !>").must_equal "<!--[if (gt IE 5)&(lt IE 7)]>"
+  #   Dang.it("<! [if (IE 6)|(IE 7)] !>").must_equal "<!--[if (IE 6)|(IE 7)]>"
+  # end
+
+  # it "transforms DANG conditional comment closers with leading content" do
   #   Dang.it("xyz<! [end] !>").must_equal      "xyz<![endif]-->"
   # end
 
-  # it "transforms one line DANG conditional comments into one line HTML conditional comments" do
+  # it "transforms one line DANG conditional comments" do
   #   Dang.it("<! [if IE] !>...<! [end] !>").must_equal "<!--[if IE]>...<![endif]-->"
   # end
 
-  # it "transforms NOT IE one line DANG conditional comments into one line HTML conditional comments" do
+  # it "transforms NOT IE one line DANG conditional comments" do
   #   Dang.it("<! [if !IE] !>...<! [end] !>").must_equal "<!--[if !IE]>...<![endif]-->"
   # end
 
