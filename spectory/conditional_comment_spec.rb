f87b3d36f70568327a195994f49ebca8a08fcbb7
Evan Phoenix <ephoenix@engineyard.com>
2011-04-12 21:05:02 -0700

    Conditional comments? We got your conditional comment right here!

--- spectory/conditional_comment_spec.rb
+++ spectory/conditional_comment_spec.rb
@@ -26,37 +26,31 @@ describe "conditional comments" do
   end
 
   it "transforms DANG conditional comment closers" do
-    Dang.it("<! [end] !>").must_equal "<![endif]-->"
+    Dang.it("<! [endif] !>").must_equal "<![endif]-->"
   end
 
   it "transforms DANG conditional comment closers with whitespace" do
-    Dang.it("<!      [end]    !>").must_equal "<![endif]-->"
+    Dang.it("<!      [endif]    !>").must_equal "<![endif]-->"
   end
 
-  # it "transforms conditional comments with boolean operators" do
-  #   Dang.it("<! [if (gt IE 5)&(lt IE 7)] !>").must_equal "<!--[if (gt IE 5)&(lt IE 7)]>"
-  #   Dang.it("<! [if (IE 6)|(IE 7)] !>").must_equal "<!--[if (IE 6)|(IE 7)]>"
-  # end
+  it "transforms conditional comments with boolean operators" do
+    Dang.it("<! [if (gt IE 5)&(lt IE 7)] !>").must_equal "<!--[if (gt IE 5)&(lt IE 7)]>"
+    Dang.it("<! [if (IE 6)|(IE 7)] !>").must_equal "<!--[if (IE 6)|(IE 7)]>"
+  end
 
-  # it "transforms DANG conditional comment closers with leading content" do
-  #   Dang.it("xyz<! [end] !>").must_equal      "xyz<![endif]-->"
-  # end
+  it "transforms DANG conditional comment closers with leading content" do
+    Dang.it("xyz <! [endif] !>").must_equal "xyz <![endif]-->"
+  end
 
-  # it "transforms one line DANG conditional comments" do
-  #   Dang.it("<! [if IE] !>...<! [end] !>").must_equal "<!--[if IE]>...<![endif]-->"
-  # end
+  it "transforms DANG conditional comment closers with directly leading content" do
+    Dang.it("xyz<! [endif] !>").must_equal "xyz<![endif]-->"
+  end
 
-  # it "transforms NOT IE one line DANG conditional comments" do
-  #   Dang.it("<! [if !IE] !>...<! [end] !>").must_equal "<!--[if !IE]>...<![endif]-->"
-  # end
+  it "transforms one line DANG conditional comments" do
+    Dang.it("<! [if IE] !>...<! [endif] !>").must_equal "<!--[if IE]>...<![endif]-->"
+  end
 
-  # it "transforms conditional comments to a particular IE version" do
-  #   Dang.it("<! [if IE5] !>...<! [end] !>").must_equal "<!--[if IE5]>...<![endif]-->"
-  #   Dang.it("<! [if IE6] !>...<! [end] !>").must_equal "<!--[if IE6]>...<![endif]-->"
-  #   Dang.it("<! [if IE7] !>...<! [end] !>").must_equal "<!--[if IE7]>...<![endif]-->"
-  #   Dang.it("<! [if IE8] !>...<! [end] !>").must_equal "<!--[if IE8]>...<![endif]-->"
-  #   Dang.it("<! [if IE9] !>...<! [end] !>").must_equal "<!--[if IE9]>...<![endif]-->"
-  # end
+  it "allows the body to dangified"
 
   # it "does all the permutations of IE conditional comments"
   # it "transforms IE conditional comments for non IE"
