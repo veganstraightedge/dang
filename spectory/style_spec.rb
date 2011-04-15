6dd9b4e4348a2810776a205bd027c45cac062c9a
Shane Becker <veganstraightedge@gmail.com>
2011-04-14 19:46:15 -0700

    failing style spec

--- spectory/style_spec.rb
+++ spectory/style_spec.rb
@@ -26,4 +26,9 @@ p:after {
 
     Dang.it(dang).must_equal html
   end
+
+  it "skips css that looks like a closing dang tag" do
+    Dang.it("<style abbr:after { content: ' style>' } style>").must_equal "<style>abbr:after { content: ' style>' } </style>"
+    Dang.it('<style abbr:after { content: " style>" } style>').must_equal '<style>abbr:after { content: " style>" } </style>'
+  end
 end
