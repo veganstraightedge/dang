e001181b5e0b3475ce4c8773a578e098d281f000
Shane Becker <veganstraightedge@gmail.com>
2013-06-04 16:47:39 -0700

    add trailing comment spec

--- spectory/comment_spec.rb
+++ spectory/comment_spec.rb
@@ -12,4 +12,8 @@ describe "comments" do
     comment
     -->"
   end
+
+  it "transforms trailing <! comments !> into html comments" do
+    Dang.it("<html html html> <! html !>").must_equal "<html>html</html> <!-- html -->"
+  end
 end
