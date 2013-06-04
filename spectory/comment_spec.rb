e19e9373e95e0a26f48bf4354b29971555ee481c
Shane Becker <veganstraightedge@gmail.com>
2013-06-04 16:50:07 -0700

    added spec for comments with bangs in them

--- spectory/comment_spec.rb
+++ spectory/comment_spec.rb
@@ -14,10 +14,10 @@ describe "comments" do
   end
 
   it "transforms trailing <! comments !> into html comments" do
-    Dang.it("<html html html> <! html !>").must_equal "<html>html</html> <!-- html -->"
+    Dang.it("<html markups html> <! html !>").must_equal "<html>markups</html> <!-- html -->"
   end
 
-  it "transforms trailing <! comments !> into html comments" do
-    Dang.it("<b bold text html> <! b! !>").must_equal "<b>bold text</b> <!-- b! -->"
+  it "transforms <! comments! !> with !s in them into html comments" do
+    Dang.it("<b bold text b> <! b! !>").must_equal "<b>bold text</b> <!-- b! -->"
   end
 end
