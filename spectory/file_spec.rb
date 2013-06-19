d648f5e515947219e17060315eb9ab58016d7d70
Shane Becker <veganstraightedge@gmail.com>
2013-06-18 17:32:27 -0700

    fixed full file spec

--- spectory/file_spec.rb
+++ spectory/file_spec.rb
@@ -2,6 +2,12 @@ require "spectory/helper"
 
 describe "files" do
   it "can transform a whole document" do
-    Dang.it( File.open("files/iamshane-com.html.dang") ).must_equal File.open("files/iamshane-com.html")
+    dang_path = File.expand_path("../files/iamshane-com.html.dang", __FILE__)
+    html_path = File.expand_path("../files/iamshane-com.html",      __FILE__)
+
+    dang = File.open(dang_path, "rb").read
+    html = File.open(html_path, "rb").read
+
+    Dang.it(dang).must_equal html
   end
 end
