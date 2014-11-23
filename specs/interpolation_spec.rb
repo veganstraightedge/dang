fceee469ebe50955cff2aa754d0fcb2906728574
Shane Becker <veganstraightedge@gmail.com>
2014-11-23 01:44:42 -0800

    Add failing attribute interpolation

--- specs/interpolation_spec.rb
+++ specs/interpolation_spec.rb
@@ -1,11 +1,11 @@
 require "specs/helper"
 
-# TODO experimental
-# for v2
+describe "interpolation" do
+  # it "should allow ruby interpolation in ids"
+  # it "should allow ruby interpolation in classes"
 
-# describe "interpolation" do
-#   it "should allow ruby interpolation in ids"
-#   it "should allow ruby interpolation in classes"
-#   it "should allow ruby interpolation in attributes"
-#   it "should allow ruby interpolation in values"
-# end
+  it "should interpolate in attributes" do
+    Dang.it("<time[datetime=<| Time.now.year |>-<| Time.now.month |>-<| Time.now.min |>] <| Time.now |> time>").must_equal
+            "<time datetime=''>#{Time.now}</time>"
+  end
+end
